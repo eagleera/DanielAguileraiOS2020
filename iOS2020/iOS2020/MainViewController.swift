5af967582f06ad9af8e2986796993c9af2a674e1
Daniel Aguilera <daguilera3220@gmail.com>
2020-01-28 19:50:41 -0700

    se agrega primer practica

---
+++ iOS2020/iOS2020/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  iOS2020
+//
+//  Created by Noel Aguilera Terrazas on 28/01/20.
+//  Copyright © 2020 Daniel Aguilera. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        btnPrimerPractica.layer.cornerRadius = 11
+        
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var btnPrimerPractica: UIButton!
+    
+}
+
